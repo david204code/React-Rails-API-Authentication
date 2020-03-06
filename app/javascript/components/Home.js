c31845c88f3ea9cee868d195e21ebffef1c1b4d7
David Wu <david204code@gmail.com>
2020-03-06 17:52:32 +0000

    laid the foundation to keep track of the state of the app

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import Registration from "./auth/Registration";
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
   render() {
     return (
       <div>
         <h1>Home</h1>
-        <h1>Home</h1>
+        <h1>Status: {this.props.loggedInStatus}</h1>
         <Registration />
       </div>
     )
